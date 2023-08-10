f43b8f3dfd0d9f18dc0971644d67f8d7ea510ada
Matthew Drnevich <mdkdrnevich@gmail.com>
2023-08-10 20:15:09 +0000

    Updated the handling of weights for events to ensure proper normalization and added 'carl_utils' to 'utils' via a symlink for importing

--- utils/carl_utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import eval
-from . import models
-from . import plotting
-from . import preprocessing
-from . import train
--- utils/config.py
+++ utils/config.py
@@ -86,7 +86,8 @@ config = {
         # name of ML model in Triton server
         "MODEL_NAME": "reconstruction_bdt_xgb",
         # URL of Triton server
-        "TRITON_URL": "agc-triton-inference-server:8001",
+        #"TRITON_URL": "agc-triton-inference-server:8001",
+        "TRITON_URL": "triton-traefik.triton.svc.cluster.local:8000",
         # Triton model version which is trained on even events
         "MODEL_VERSION_EVEN": "2",
         # Triton model version which is trained on odd events
